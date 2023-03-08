b002d016e11a7e6ba3e2e458a6a121ac93cbcdc4
DominicS99 <98355617+DominicS99@users.noreply.github.com>
2023-03-08 07:12:22 -0800

    Recursive version using only lists

--- Leetcode/0021: Merge Two Sorted Lists.cpp
+++ Leetcode/0021: Merge Two Sorted Lists.cpp
@@ -11,47 +11,13 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        vector<int> a1, a2, res;
-        while (list1) {
-            a1.push_back(list1->val);
-            list1 = list1->next;
+        if (!list1) return list2;
+        if (!list2) return list1;
+        if (list1->val > list2->val) {
+            list2->next = mergeTwoLists(list1, list2->next);
+            return list2;
         }
-
-        while (list2) {
-            a2.push_back(list2->val);
-            list2 = list2->next;
-        }
-
-        int l = 0, r = 0;
-        while (l < a1.size() && r < a2.size()) {
-            if (a1[l] < a2[r]) {
-                res.push_back(a1[l]);
-                l++;
-            } else {
-                res.push_back(a2[r]);
-                r++;
-            }
-        }
-
-        while (l < a1.size()) {
-            res.push_back(a1[l]);
-            l++;
-        }
-        while (r < a2.size()) {
-            res.push_back(a2[r]);
-            r++;
-        }
-
-        if (res.empty()) return nullptr;
-
-        ListNode * root = new ListNode(res[0]);
-        ListNode * prev = root;
-        for (int i = 1; i < res.size(); i++) {
-            ListNode * curr = new ListNode(res[i]);
-            prev->next = curr;
-            prev = curr;
-        }
-
-        return root;
+        list1->next = mergeTwoLists(list1->next, list2);
+        return list1;
     }
 };
