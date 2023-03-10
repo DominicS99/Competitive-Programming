ecf5eb4cedd402b1f22bbce1ae788d60ec0bfb86
DominicS99 <98355617+DominicS99@users.noreply.github.com>
2023-03-10 15:03:11 -0800

    Create 0724: Find Pivot Index.cpp

---
+++ Leetcode/0724: Find Pivot Index.cpp
@@ -0,0 +1,16 @@
+typedef long long LL;
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n = nums.size();
+        LL l = 0, r = accumulate(nums.begin(), nums.end(), 0);
+
+        for (int i = 0; i < n; i++) {
+            r -= nums[i];
+            if (l == r) return i;
+            l += nums[i];
+        }
+
+        return -1;
+    }
+};
