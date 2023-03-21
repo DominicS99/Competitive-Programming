d5f2703bc941775e2ba216e64f238b16a10c2e82
DominicS99 <98355617+DominicS99@users.noreply.github.com>
2023-03-21 13:01:15 -0700

    Update 0088: Merge Sorted Array.cpp

--- Leetcode/0088: Merge Sorted Array.cpp
+++ Leetcode/0088: Merge Sorted Array.cpp
@@ -15,3 +15,17 @@ public:
         }
     }
 };
+
+// Secret Santa #1 solution
+/*
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int r = nums1.size()-1;
+        m--, n--;
+        while (n >= 0) {
+            nums1[r--] = (m >= 0 && nums1[m] > nums2[n]) ? nums1[m--] : nums2[n--];
+        }
+    }
+};
+*/
