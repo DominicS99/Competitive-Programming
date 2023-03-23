cf5de564ab938081da999152a100b35dd890ecac
DominicS99 <98355617+DominicS99@users.noreply.github.com>
2023-03-23 15:09:03 -0700

    Create 0169: Majority Element.cpp

---
+++ Leetcode/0169: Majority Element.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size()/2;
+        unordered_map<int, int> m;
+        for (auto &it : nums) m[it]++;
+        for (auto &it : m) {
+            if (it.second > n) return it.first; 
+        }
+        return -1;
+    }
+};
